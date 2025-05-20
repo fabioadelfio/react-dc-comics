676d3b3a12133101e24d3296c11312d8542b223b
fabioadelfio <adelfiofabio07@gmail.com>
2025-05-20 18:42:18 +0200

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,145 @@
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <footer>
+            <div id="page-info">
+                <div className="container">
+                        <div className="links">
+                            <div className="col-1-3">
+                                <div className="title-info">DC COMICS</div>
+                                <ul>
+                                    <li>
+                                        <a href="#">Characters</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Comics</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Movies</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">TV</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Games</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Videos</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">News</a>
+                                    </li>
+
+                                </ul>
+                                <div className="title-info">SHOP</div>
+                                <ul>
+                                    <li>
+                                        <a href="#">Shop DC</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Shop DC Collectibles</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-1-3">
+                                <div className="title-info">DC</div>
+                                <ul>
+                                    <li>
+                                        <a href="#">Terms Of Use</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Privacy Policy (New)</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ad Choices</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Advertising</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Jobs</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Subscriptions</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Talent Workshops</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">CPSC Certificates</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ratings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Shop Help</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-1-3">
+                                <div className="title-info">SITES</div>
+                                <ul>
+                                    <li>
+                                        <a href="#">DC</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">MAD Magazine</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Kids</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Universe</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Power Visa</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="logo-footer">
+                            
+                        </div>
+                </div>
+
+                <div id="page-footer">
+                    <div className="container">
+                        <div className="sign-up">SIGN-UP NOW!</div>
+
+                        <div className="social">
+                            <div>FOLLOW US</div>
+                            <div>
+                                <a href="#">
+                                    <img src="./footer-facebook.png" alt="fb" />
+                                </a>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <img src="./footer-twitter.png" alt="fb" />
+                                </a>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <img src="./footer-youtube.png" alt="fb" />
+                                </a>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <img src="./footer-pinterest.png" alt="fb" />
+                                </a>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <img src="./footer-periscope.png" alt="fb" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 }
